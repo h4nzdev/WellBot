33968be65cf2a94911e05cefe7243ac7076d5924
Cmhanz99 <hanzhmagbal@gmail.com>
2025-09-12 23:55:25 +0800

    my commit

--- frontend/src/routes/ClientRoutes.jsx
+++ frontend/src/routes/ClientRoutes.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientLayout from "../layout/ClientLayout";
 import ClientDashboard from "../page/ClientPages/ClientDashboard/ClientDashboard";
 
 const ClientRoutes = () => {
   return (
-    <Routes>
-      <Route path="*" element={<Navigate to="/client/dashboard" />} />
-      <Route
-        path="/client/dashboard"
-        element={
-          <ClientLayout>
-            <ClientDashboard />
-          </ClientLayout>
-        }
-      />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navigate to="/client/dashboard" />} />
+        <Route
+          path="/client/dashboard"
+          element={
+            <ClientLayout>
+              <ClientDashboard />
+            </ClientLayout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
