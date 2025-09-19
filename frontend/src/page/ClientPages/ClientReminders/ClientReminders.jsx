16fc6de8e571c4e280fc238b038264f56e847f54
Cmhanz99 <hanzhmagbal@gmail.com>
2025-09-19 08:52:09 +0800

    .

--- frontend/src/page/ClientPages/ClientReminders/ClientReminders.jsx
+++ frontend/src/page/ClientPages/ClientReminders/ClientReminders.jsx
@@ -129,9 +129,7 @@ const ClientReminders = () => {
               onClick={openModal}
               className="group flex items-center justify-center px-6 py-3 bg-cyan-600/90 text-white rounded-xl shadow-lg hover:bg-cyan-700 hover:scale-105 transition-all duration-300 w-full sm:w-auto"
             >
-              <Plus
-                className="w-5 h-5 mr-2 group-hover:rotate-90 transition-transform duration-300"
-              />
+              <Plus className="w-5 h-5 mr-2 group-hover:rotate-90 transition-transform duration-300" />
               Set New Reminder
             </button>
           </header>
@@ -278,7 +276,7 @@ const ClientReminders = () => {
       </div>
 
       {isNotificationModalOpen && dueReminder && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
           <div className="bg-white p-8 rounded-lg shadow-2xl transform transition-all scale-100">
             <h2 className="text-3xl font-bold mb-4 text-slate-800">
               Reminder!
